b2d00cfc9ddb1639e1b38cfbcc7bab183b51971b
quasur <adam.corness@gmail.com>
2024-02-05 13:45:33 +0000

    look up table

---
+++ detection.py
@@ -0,0 +1,44 @@
+#%%
+import numpy as np
+from scipy.stats import chisquare
+#import dataset
+
+kdat = np.loadtxt("data/kdata.npy").reshape((114243,38,3))
+jdat = np.loadtxt("data/jdata.npy").reshape((114243,35,3))
+
+
+kydat = np.loadtxt("data/kydata.npy").reshape((114243,7,3))
+jydat = np.loadtxt("data/jydata.npy").reshape((114243,8,3))
+
+"""
+chisq=np.zeros((kdat[:,0,1].size))
+for i in range(kdat[:,0,0].size):
+    expval = np.mean(kdat[i,:,1])
+    chisq[i] = np.sum((kdat[i,:,1]-expval)**2/expval)
+
+"""
+
+chisq = chisquare(kydat[:,:,1],axis=1)[0]
+
+#%%
+import matplotlib.pyplot as plt
+plt.hist(np.log(chisq),bins=1000)
+plt.show()
+plt.plot(chisq)
+plt.show()
+
+std = np.std(chisq)
+mean = np.mean(chisq)
+
+big = np.where(np.logical_and(chisq>100*std+mean,True))#,chisq<10000000*std+mean))
+kdatbig = kdat[big]
+kydatbig=kydat[big]
+chisqlst = chisq[big]
+indx = np.arange(kdat[:,0,0].size)[big]
+
+for i in range(kdatbig[:,0,0].size):
+    plt.plot(kdatbig[i,:,0],kdatbig[i,:,1],'.')
+    plt.errorbar(kdatbig[i,:,0],kdatbig[i,:,1],kdatbig[i,:,2],color='blue',linestyle='')
+    plt.plot((kydatbig[i,:,0]-2005)*12,kydatbig[i,:,1],'-')
+    print((chisqlst[i]-mean)/std,indx[i])
+    plt.show()
--- import.py
+++ import.py
@@ -37,3 +37,43 @@ np.savetxt("data/kdata.npy",kdata.reshape(kdata.shape[0],-1))
 #Use these lines to import jdata and kdata in other scripts:
 #np.loadtxt("data/kdata.npy").reshape((114243,38,3))
 #np.loadtxt("data/jdata.npy").reshape((114243,35,3))
+
+#%% semersterly data
+#%%
+from astropy.io import fits
+from astropy.table import table
+import numpy as np
+
+f = fits.open("data/semester_lightcurves_J_and_K.fits")
+fitsdata = np.array(f[1].data)
+messdata = np.array(fitsdata.tolist())
+
+kdata = np.zeros((114243,7,3))
+jdata = np.zeros((114243,8,3))
+
+#format k data
+for j in range(7):#38 for month
+    i=j
+    kdata[:,j,1] = messdata[:,i*2+3]
+    kdata[:,j,2] = messdata[:,i*2+4]
+
+#format j data
+for j in range(8):#35 for month
+    i=j+7#38 for month
+    jdata[:,j,1] = messdata[:,i*2+3]
+    jdata[:,j,2] = messdata[:,i*2+4]
+
+#import observation time
+Jx = np.array([2005,2006,2007,2008,2009,2010,2011,2012])#np.loadtxt("data/J_band_x-axis.txt")
+Kx = np.array([2005,2007,2008,2009,2010,2011,2012])#np.loadtxt("data/K_band_x-axis.txt")
+
+#append to main array
+jdata[:,:,0]=Jx
+kdata[:,:,0]=Kx
+
+#save data as 2d array
+np.savetxt("data/jydata.npy",jdata.reshape(jdata.shape[0],-1))
+np.savetxt("data/kydata.npy",kdata.reshape(kdata.shape[0],-1))
+
+#np.loadtxt("data/kdata.npy").reshape((114243,7,3))
+#np.loadtxt("data/jdata.npy").reshape((114243,8,3))
