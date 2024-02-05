2395190181497259f022d4bca3bc3c7bbcd8d0ec
quasur <adam.corness@gmail.com>
2024-02-05 11:30:22 +0000

    chi squared working with UDS data

--- detection.py
+++ detection.py
@@ -5,6 +5,11 @@ from scipy.stats import chisquare
 
 kdat = np.loadtxt("data/kdata.npy").reshape((114243,38,3))
 jdat = np.loadtxt("data/jdata.npy").reshape((114243,35,3))
+
+
+kydat = np.loadtxt("data/kydata.npy").reshape((114243,7,3))
+jydat = np.loadtxt("data/jydata.npy").reshape((114243,8,3))
+
 """
 chisq=np.zeros((kdat[:,0,1].size))
 for i in range(kdat[:,0,0].size):
@@ -25,8 +30,15 @@ plt.show()
 std = np.std(chisq)
 mean = np.mean(chisq)
 
-big = np.where(np.logical_and(chisq>10*std+mean,chisq<16*std+mean))
+big = np.where(np.logical_and(chisq>10*std+mean,True))#,chisq<10000000*std+mean))
 kdatbig = kdat[big]
+kydatbig=kydat[big]
+chisqlst = chisq[big]
+indx = np.arange(kdat[:,0,0].size)[big]
 
-for i in range(3):#kdatbig[:,0,0].size):
-    plt.plot(kdatbig[i,:,0],kdatbig[i,:,1],'-')
+for i in range(kdatbig[:,0,0].size):
+    plt.plot(kdatbig[i,:,0],kdatbig[i,:,1],'.')
+    plt.errorbar(kdatbig[i,:,0],kdatbig[i,:,1],kdatbig[i,:,2],color='blue',linestyle='')
+    plt.plot((kydatbig[i,:,0]-2005)*12,kydatbig[i,:,1],'-')
+    print((chisqlst[i]-mean)/std,indx[i])
+    plt.show()
